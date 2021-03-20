43ae57a01fce0fe7ff36f403a8c1147280306565
Richard Kasperowski <r@kasperowski.com>
2021-03-20 16:42:39 -0400

    Update RomanUnitTests.cpp

--- test/RomanUnitTests.cpp
+++ test/RomanUnitTests.cpp
@@ -1,2 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
 #include "catch.hpp"
+
+TEST_CASE("The Catch test framework works properly")
+{
+    REQUIRE(42 == 6 * 7);
+}
